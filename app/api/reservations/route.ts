951e386b29bf0ea1d61e98bf9295203cc694a0ff
charlie-react <charles49ja@gmail.com>
2023-10-20 17:51:41 +0100

    fixed errors

--- app/api/reservations/route.ts
+++ app/api/reservations/route.ts
@@ -70,7 +70,7 @@ export async function POST(
       id: listingId
     },
     data: {
-      reservation: {
+      reservations: {
         create: {
           userId: currentUser.id,
           startDate,
