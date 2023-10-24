f1253e499a5618615101ef761ee272683d5e1efa
charlie-react <charles49ja@gmail.com>
2023-10-24 11:12:44 +0100

    uncomment

--- app/api/reservations/route.ts
+++ app/api/reservations/route.ts
@@ -74,7 +74,7 @@ export async function POST(
       id: listingId
     },
     data: {
-      reservation: {
+      reservations: {
         create: {
           userId: currentUser.id,
           startDate,
--- app/listings/[listingId]/ListingClient.tsx
+++ app/listings/[listingId]/ListingClient.tsx
@@ -218,24 +218,25 @@ const ListingClient: React.FC<ListingClientProps> = ({
     }
     setIsLoading(true);
 
-    axios
-      .post("/api/reservations", {
-        totalPrice,
-        startDate: dateRange.startDate,
-        endDate: dateRange.endDate,
-        listingId: listing?.id,
-      })
-      .then(() => {
-        toast.success("Listing reserved!");
-        setDateRange(initialDateRange);
-        router.push("/trips");
-      })
-      .catch(() => {
-        toast.error("Something went wrong.");
-      })
-      .finally(() => {
-        setIsLoading(false);
-      });
+    // axios
+    //   .post("/api/reservations", {
+    //     totalPrice,
+    //     startDate: dateRange.startDate,
+    //     endDate: dateRange.endDate,
+    //     listingId: listing?.id,
+    //   })
+    //   .then(() => {
+    //     toast.success("Listing reserved!");
+    //     setDateRange(initialDateRange);
+    //     router.push("/trips");
+    //   })
+    //   .catch((error) => {
+    //     toast.error("Something went wrong.");
+    //     console.log(error)
+    //   })
+    //   .finally(() => {
+    //     setIsLoading(false);
+    //   });
   }, [totalPrice, dateRange, listing?.id, router, currentUser, loginModal]);
 
   useEffect(() => {
